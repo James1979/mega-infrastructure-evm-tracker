1cb215ac777959c029cf6d112b836f988f8aa5d7
James Lim <mortgagespecialist1979@gmail.com>
2025-08-09 14:55:48 -0700

    Added base Streamlit app

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
+# -*- coding: utf-8 -*-
 import streamlit as st
 
-st.set_page_config(page_title='Mega-Infrastructure EVM Tracker', layout='wide')
+st.set_page_config(page_title="Mega-Infrastructure EVM Tracker", layout="wide")
 
-st.title('íº§ Mega-Infrastructure EVM Tracker')
-st.write('Welcome! This is your live project controls dashboard.')
-
-
+st.title("Mega-Infrastructure EVM Tracker")
+st.write("Welcome! This is your live project controls dashboard.")
